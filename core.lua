a18a35c86bbe9a2310fae4c19b8c453d84341f31
Hiraz <76824983+DerHiraz@users.noreply.github.com>
2021-01-17 02:48:24 +0100

    Update core.lua

--- core.lua
+++ core.lua
@@ -1,3 +1,12 @@
 LucidKeystone = LibStub("AceAddon-3.0"):NewAddon("LucidKeystone")
 --LucidKeystone.frame = CreateFrame("Frame", "LucidKeystoneFrame", UIParent)
 
+--    _           _    _ 
+--   | |  _  _ __(_)__| |
+--   | |_| || / _| / _` |
+--   |____\_,_\__|_\__,_|
+--   | |/ /___ _  _ __| |_ ___ _ _  ___
+--   | ' </ -_) || (_-<  _/ _ \ ' \/ -_)
+--   |_|\_\___|\_, /__/\__\___/_||_\___|
+--             |__/
+
